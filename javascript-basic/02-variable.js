64f6e5520c1412a2042dea9fe47c5d4d61f22d3b
joyalshaji135 <joyalshaji135@gmail.com>
2024-03-08 07:53:02 +0530

    Basic Started

---
+++ javascript-basic/02-variable.js
@@ -0,0 +1,34 @@
+var num = 100; // integer variable
+var names = "Joyal Shaji"; // String Variable
+
+console.log("num Variable Value is :"+num);
+console.log("name Variable Value is :"+names);
+
+var boolean = true; // Status Updating 
+
+console.log("Boolean Variable Value is :"+boolean);
+
+var name1 = new String("Glorie Is Good");
+
+console.log("name variable length is :"+names.length);
+
+console.log("name1 variable value is :"+name1);
+
+console.log("name1 length is :"+name1.length);
+
+var boolean1 = new Boolean(false);
+
+console.log("Boolean Value is :"+boolean1);
+
+normal_variable = 1000;
+
+console.log("Normal Variable Value is :"+normal_variable);
+
+// num Variable Value is :100
+// name Variable Value is :Joyal Shaji
+// Boolean Variable Value is :true
+// name variable length is :11
+// name1 variable value is :Glorie Is Good
+// name1 length is :14
+// Boolean Value is :false
+// Normal Variable Value is :1000
