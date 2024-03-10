c4869a088636dd684a468839be81822d80538868
joyalshaji135 <joyalshaji135@gmail.com>
2024-03-11 00:09:51 +0530

    function is started

--- javascript-basic/07-looping-statment.js
+++ javascript-basic/07-looping-statment.js
@@ -1,6 +1,19 @@
 let limit = 10;
 for(i=1; i<=limit; i++)
 {
-    console.log(i);
+    // console.log(i);
 }
 
+let str = "";
+for(i=1; i<=5; i++)
+{
+    for(j=1; j<=5; j++)
+    {
+        if(i>=j)
+        {
+            str += "*";
+        }
+    }
+    str += "\n"
+}
+console.log(str);
