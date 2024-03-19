15a1d6148f24c39cb264c1593a1e3c8a5a5aab8d
joyalshaji135 <joyalshaji135@gmail.com>
2024-03-19 22:42:38 +0530

    Function Javascript

--- javascript-basic/06-function.js
+++ javascript-basic/06-function.js
@@ -5,9 +5,22 @@ function hello()
 
 hello()
 
-function hollo()
+// Function Return Value is getting
+function fun1()
 {
-    console.log();
-} 
+    console.log("Function 1");
+    return 10;
+}
 
-hollo()
+value1 = fun1();
+console.log("Functional Value :"+value1);
+
+// Argument Function 
+
+function fun2(num1,num2)
+{
+    return num1+num2;
+}
+
+value2 = fun2(10,20);
+console.log("Sum of Two Numbers :"+value2);
